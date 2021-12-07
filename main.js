239283122a1d35901eabff7ec89fccd6f618fd3f
Howe <42908021+Howe-ya@users.noreply.github.com>
2021-12-07 20:16:53 +0800

    Update main.js

--- main.js
+++ main.js
@@ -17,3 +17,34 @@ document.addEventListener('visibilitychange', function () {
         }, 2000);
     }
 });
+
+
+function toggleSidebar(){
+        $(".layui-col-md3 .darcyq-sidebar-card").toggle(500, function () {
+            if($(".layui-col-md3 .darcyq-sidebar-card").css('display')=='none'){
+                $(".layui-container .layui-col-md8").attr("class","layui-col-md12 layui-col-xs12 layui-col-sm12 darcyq-margin-top-bottom-x2");
+                return;
+            }
+        });
+        $(".layui-container .layui-col-md8").attr("class","layui-col-md8 layui-col-xs12 layui-col-sm12 darcyq-margin-top-bottom-x2");
+    }
+    $(function () {
+        $(".darcyq-basic-info").mouseover(function(){
+            $(".toggle-sidebar-btn").show();
+            $(".darcyq-basic-info").mouseout(function(){
+                $(".toggle-sidebar-btn").hide();
+            });
+        });
+    });
+    $("#showMobileMenu").click(function () {
+        $(".darcyq-header-mobile").toggle();
+    });
+    document.onkeydown = function (e) {
+        var ev = document.all ? window.event : e;
+        if (ev.keyCode == 13) {
+            if ($('#header-hidden-box').is(':hidden')) {
+                return;
+            }
+            $("#searchBtn").trigger("click");
+        }
+    }
